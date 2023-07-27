5fddb270e0320a8e5f0b1fd26a6b09eb569d61db
KusumaVarimadugu <19svdc2162svdegreecollegeac.in@gmail.com>
2023-07-27 23:19:40 +0530

    created Driver class

--- calci.java
+++ calci.java
@@ -1,3 +1,4 @@
+import java.util.scanner;
 public interface calci
 {
 public int getsum(int n1,int n2);
@@ -6,5 +7,17 @@ class calciImpl implements calci
 {
 public int getsum(int n1,int n2){
 return n1+n2;
+}
+}
+public class calciDriver
+{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter 2 nos to find sum");
+int n1=sc.nextInt();
+int n2=sc.nextInt();
+calciImpl c=new calciImpl();
+int sum=c.getsum(n1,n2);
+System.out.println(sum);
 }
 }
